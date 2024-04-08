2dd1ca3182d4f13e57d0cd09935ff3e6c2e43673
A-Knio <adamknio6@gmail.com>
2024-04-08 13:13:54 -0400

    models added with user,index, and thoghts

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { schema, model } = require('mongoose');
+
+const UserSchema = new schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+    },
+    thoughts: [
+        {
+            type: schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false
+});
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
+
